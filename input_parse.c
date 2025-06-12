5204a237b09c3179473bcc9297f1fb9a2dd684fd
Marcos <marcos29diciembre@gmail.com>
2025-06-12 22:24:58 +0200

    final parse

--- input_parse.c
+++ input_parse.c
@@ -6,56 +6,91 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 10:56:59 by matoledo          #+#    #+#             */
-/*   Updated: 2025/06/12 12:27:22 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/06/12 22:28:09 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	add_numbers(t_list **lst, char **pt_aux)
+int	check_sorted(t_list *lst)
+{
+	t_list	*pt_aux;
+
+	pt_aux = lst;
+	pt_aux = pt_aux->next;
+	while (pt_aux)
+	{
+		if (*(int *)pt_aux->content < *(int *)lst->content)
+			return (1);
+		pt_aux = pt_aux->next;
+		lst = lst->next;
+	}
+	return (0);
+}
+
+int	check_input(char *num)
+{
+	if (*num == '-' || *num == '+')
+		num++;
+	while (*num)
+	{
+		if (ft_isdigit(*num) == 0
+			&& !((*num >= 9 && *num <= 13) || *num == 32))
+			return (0);
+		num++;
+	}
+	return (1);
+}
+
+int	check_repeated(t_list *lst, int num)
+{
+	while (lst)
+	{
+		if (*(int *)lst->content == num)
+			return (0);
+		lst = lst->next;
+	}
+	return (1);
+}
+
+int	add_number(t_list **lst, char *pt_aux)
 {
 	int		*aux;
 	t_list	*node;
 
-	while (*pt_aux)
+	aux = malloc(sizeof(int));
+	*aux = ft_atoi(pt_aux);
+	if (check_repeated(*lst, *aux) == 0)
 	{
-		if (check_bigger_int(*pt_aux) == 0)
-			return (0);
-		aux = malloc(sizeof(int));
-		*aux = ft_atoi(*pt_aux);
-		if (check_repeated(*lst, *aux) == 0)
-		{
-			free(aux);
-			return (0);
-		}
-		node = ft_lstnew(aux);
-		ft_lstadd_back(lst, node);
-		pt_aux++;
+		free(aux);
+		return (0);
 	}
+	node = ft_lstnew(aux);
+	ft_lstadd_back(lst, node);
 	return (1);
 }
 
 int	fill_list(t_list **lst, char **s)
 {
 	char	**pt_aux;
+	char	**pt_free;
 
 	while (*s)
 	{
-		if (check_input(*s) == 0)
-		{
-			ft_lstclear(lst, del_number);
-			free(lst);
-			return (0);
-		}
 		pt_aux = ft_split(*s, ' ');
-		if (add_numbers(lst, pt_aux) == 0)
+		pt_free = pt_aux;
+		while (*pt_aux)
 		{
-			free_memory(pt_aux);
-			ft_lstclear(lst, del_number);
-			free(lst);
-			return (0);
+			if (check_input(*pt_aux) == 0 || add_number(lst, *pt_aux) == 0)
+			{
+				free_memory(pt_aux);
+				ft_lstclear(lst, del_number);
+				free(lst);
+				return (0);
+			}
+			pt_aux++;
 		}
-		free_memory(pt_aux);
+		free_memory(pt_free);
 		s++;
 	}
 	return (1);
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 19:18:51 by matoledo          #+#    #+#             */
-/*   Updated: 2025/06/12 12:47:16 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/06/12 22:27:50 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ int	main(int argc, char **argv)
 			ft_printf("Error\n");
 			exit(1);
 		}
-		if (check_sorted(*a) == 0)
+		if (!(*a) || check_sorted(*a) == 0)
 		{
 			ft_lstclear(a, del_number);
 			free(a);
 			exit(0);
 		}
 		b = ft_calloc(sizeof(t_list *), 1);
-		prepare_sort_list(a, b);
+		prepare_sort_list(a, b);	
 		ft_lstclear(a, del_number);
 		free(a);
 		free(b);
